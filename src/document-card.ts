79faa339ffadbc778ce525ed903d69088dd634a1
Ahmad Khateeb <Ahmad.Khateeb@exlibrisgroup.com>
2024-09-26 11:52:30 +0300

    clean code

--- src/document-card.ts
+++ src/document-card.ts
@@ -37,15 +37,6 @@ export class DocumentCard extends LitElement {
 
         `
     }
-
-    // private getRandomColor() {
-    //     const letters = '0123456789ABCDEF';
-    //     let color = '#';
-    //     for (let i = 0; i < 6; i++) {
-    //         color += letters[Math.floor(Math.random() * 16)];
-    //     }
-    //     return color;
-    // }
 }
 
 
